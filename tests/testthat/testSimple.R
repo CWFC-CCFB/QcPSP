5892f976793b409668a81d8d01e780a78cab853c
donenrique24 <mathieu.fortin.re@gmail.com>
2024-01-03 13:33:19 -0500

    Adding metadata to the project

--- tests/testthat/testSimple.R
+++ tests/testthat/testSimple.R
@@ -8,3 +8,8 @@ test_that("Testing nb rows in QcPlotIndex", {expect_equal(nrow(QcPlotIndex), 128
 test_that("Testing nb rows in QcTreeIndex", {expect_equal(nrow(QcTreeIndex), 814232)})
 test_that("Testing nb rows in QcMeasurementIndex", {expect_equal(nrow(QcMeasurementIndex), 50430)})
 test_that("Testing nb rows in QcTreeMeasurements", {expect_equal(nrow(QcTreeMeasurements), 2089280)})
+
+test_that("Testing nb rows in metadata of QcPlotIndex", {expect_equal(nrow(getMetaData(QcPlotIndex)), 17)})
+test_that("Testing nb rows in metadata of QcTreeIndex", {expect_equal(nrow(getMetaData(QcTreeIndex)), 9)})
+test_that("Testing nb rows in metadata of QcMeasurementIndex", {expect_equal(nrow(getMetaData(QcMeasurementIndex)), 12)})
+test_that("Testing nb rows in metadata of QcTreeMeasurements", {expect_equal(nrow(getMetaData(QcTreeMeasurements)), 5)})
