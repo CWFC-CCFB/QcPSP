c613c63a5de6a4731c95650c4cabe745a9a75f99
Mathieu Fortin <mathieu.fortin.re@gmail.com>
2024-06-26 15:35:56 -0400

    Adding all plots into QcSBWDefoliation.Rds

--- tests/testthat/testSimple.R
+++ tests/testthat/testSimple.R
@@ -9,7 +9,7 @@ test_that("Testing nb rows in QcTreeIndex", {expect_equal(nrow(QcTreeIndex), 814
 test_that("Testing nb rows in QcMeasurementIndex", {expect_equal(nrow(QcMeasurementIndex), 50430)})
 test_that("Testing nb rows in QcTreeMeasurements", {expect_equal(nrow(QcTreeMeasurements), 2089280)})
 test_that("Testing nb rows in QcSaplingMeasurements", {expect_equal(nrow(QcSaplingMeasurements), 223475)})
-test_that("Testing nb rows in QcSaplingMeasurements", {expect_equal(nrow(QcSBWDefoliation), 10815)})
+test_that("Testing nb rows in QcSaplingMeasurements", {expect_equal(nrow(QcSBWDefoliation), 12816)})
 
 test_that("Testing nb rows in metadata of QcPlotIndex", {expect_equal(nrow(getMetaData(QcPlotIndex)), 17)})
 test_that("Testing nb rows in metadata of QcTreeIndex", {expect_equal(nrow(getMetaData(QcTreeIndex)), 9)})
